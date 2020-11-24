5d87ff4271e30602fac718046e4fb3c446404a11
Isabelle Christine Bertanha <isaa_christinee@hotmail.com>
2020-11-24 18:09:05 -0300

    COM120 - Semana 7 Feitos exemplos da vídeoaula 19

---
+++ COM120 - Semana 7/aula19_botaodata.py
@@ -0,0 +1,14 @@
+from tkinter import Tk, Button
+from tkinter.messagebox import showinfo
+from time import strftime, localtime
+
+
+def clicked():
+    time = strftime('Day: %d %b %Y\nTime: %H:%M:%S%p\n', localtime())
+    showinfo(message=time)
+
+
+root = Tk()
+button = Button(master=root, text='Clique', command=clicked)
+button.pack()
+root.mainloop()
